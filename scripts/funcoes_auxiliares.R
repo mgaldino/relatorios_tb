dcab660936cf3f32592e5e50391a609bc3637db2
mgaldino <mcz.fea@gmail.com>
2016-11-17 17:27:34 -0200

    auxiliares

---
+++ scripts/funcoes_auxiliares.R
@@ -0,0 +1,66 @@
+candidate_fed1 <- function (year) 
+{
+  test_fed_year(year)
+  dados <- tempfile()
+  sprintf("http://agencia.tse.jus.br/estatistica/sead/odsele/consulta_cand/consulta_cand_%s.zip", 
+          year) %>% download.file(dados)
+  unzip(dados, exdir = paste0("./", year))
+  unlink(dados)
+  cat("Processing the data...")
+  setwd(as.character(year))
+  banco <- juntaDados1()
+  setwd("..")
+  unlink(as.character(year), recursive = T)
+  if (year < 2014) {
+    names(banco) <- c("DATA_GERACAO", "HORA_GERACAO", "ANO_ELEICAO", 
+                      "NUM_TURNO", "DESCRICAO_ELEICAO", "SIGLA_UF", "SIGLA_UE", 
+                      "DESCRICAO_UE", "CODIGO_CARGO", "DESCRICAO_CARGO", 
+                      "NOME_CANDIDATO", "SEQUENCIAL_CANDIDATO", "NUMERO_CANDIDATO", 
+                      "CPF_CANDIDATO", "NOME_URNA_CANDIDATO", "COD_SITUACAO_CANDIDATURA", 
+                      "DES_SITUACAO_CANDIDATURA", "NUMERO_PARTIDO", "SIGLA_PARTIDO", 
+                      "NOME_PARTIDO", "CODIGO_LEGENDA", "SIGLA_LEGENDA", 
+                      "COMPOSICAO_LEGENDA", "NOME_COLIGACAO", "CODIGO_OCUPACAO", 
+                      "DESCRICAO_OCUPACAO", "DATA_NASCIMENTO", "NUM_TITULO_ELEITORAL_CANDIDATO", 
+                      "IDADE_DATA_ELEICAO", "CODIGO_SEXO", "DESCRICAO_SEXO", 
+                      "COD_GRAU_INSTRUCAO", "DESCRICAO_GRAU_INSTRUCAO", 
+                      "CODIGO_ESTADO_CIVIL", "DESCRICAO_ESTADO_CIVIL",
+                      "CODIGO_NACIONALIDADE", "DESCRICAO_NACIONALIDADE", 
+                      "SIGLA_UF_NASCIMENTO", "CODIGO_MUNICIPIO_NASCIMENTO", 
+                      "NOME_MUNICIPIO_NASCIMENTO", "DESPESA_MAX_CAMPANHA", 
+                      "COD_SIT_TOT_TURNO", "DESC_SIT_TOT_TURNO")
+  }
+  else {
+    names(banco) <- c("DATA_GERACAO", "HORA_GERACAO", "ANO_ELEICAO", 
+                      "NUM_TURNO", "DESCRICAO_ELEICAO", "SIGLA_UF", "SIGLA_UE", 
+                      "DESCRICAO_UE", "CODIGO_CARGO", "DESCRICAO_CARGO", 
+                      "NOME_CANDIDATO", "SEQUENCIAL_CANDIDATO", "NUMERO_CANDIDATO", 
+                      "CPF_CANDIDATO", "NOME_URNA_CANDIDATO", "COD_SITUACAO_CANDIDATURA", 
+                      "DES_SITUACAO_CANDIDATURA", "NUMERO_PARTIDO", "SIGLA_PARTIDO", 
+                      "NOME_PARTIDO", "CODIGO_LEGENDA", "SIGLA_LEGENDA", 
+                      "COMPOSICAO_LEGENDA", "NOME_COLIGACAO", "CODIGO_OCUPACAO", 
+                      "DESCRICAO_OCUPACAO", "DATA_NASCIMENTO", "NUM_TITULO_ELEITORAL_CANDIDATO", 
+                      "IDADE_DATA_ELEICAO", "CODIGO_SEXO", "DESCRICAO_SEXO", 
+                      "COD_GRAU_INSTRUCAO", "CODIGO_COR_RACA", "DESCRICAO_COR_RACA", 
+                      "DESCRICAO_GRAU_INSTRUCAO", "CODIGO_ESTADO_CIVIL", 
+                      "DESCRICAO_ESTADO_CIVIL", "CODIGO_NACIONALIDADE", 
+                      "DESCRICAO_NACIONALIDADE", "SIGLA_UF_NASCIMENTO", 
+                      "CODIGO_MUNICIPIO_NASCIMENTO", "NOME_MUNICIPIO_NASCIMENTO", 
+                      "DESPESA_MAX_CAMPANHA", "COD_SIT_TOT_TURNO", "DESC_SIT_TOT_TURNO", 
+                      "EMAIL_CANDIDATO")
+  }
+  cat("Done")
+  return(banco)
+}
+
+
+
+juntaDados1 <- function () 
+{
+  banco <- Sys.glob("*.txt") %>% lapply(function(x) tryCatch(read.table(x, 
+                                                                        header = F, sep = ";",colClasses = "character", stringsAsFactors = F, fill = T, 
+                                                                        fileEncoding = "windows-1252"), error = function(e) NULL))
+  nCols <- sapply(banco, ncol)
+  banco <- banco[nCols == Moda(nCols)] %>% do.call("rbind", 
+                                                   .)
+  banco
+}
---
+++ scripts/funcoes_auxiliares_ggplot.R
@@ -0,0 +1,37 @@
+real_format <- function(prefix = "$", suffix = "", largest_with_cents = 100000,
+                        ..., big.mark = ".", negative_parens = FALSE) {
+  require(reshape)
+  require(stringr) 
+  
+  function(x) {
+    x <- round_any(x, 0.01)
+    if (max(x, na.rm = TRUE) < largest_with_cents &
+        !all(x == floor(x), na.rm = TRUE)) {
+      nsmall <- 2L
+    } else {
+      x <- round_any(x, 1)
+      nsmall <- 0L
+    }
+    str_c("R$ ", format(x, nsmall = nsmall, trim = TRUE, big.mark = ".", 
+                      decimal.mark = ",", scientific = FALSE, digits=1L))
+  }
+}
+
+
+
+theme_tb <- function (base_size = 12, base_family = "Helvetica",
+                      legend.position="bottom", legend_size= 10) {
+  theme_grey(base_size = base_size, base_family = base_family) %+replace% 
+    theme(legend.position = legend.position , 
+          legend.margin=unit(-.2,"cm"),
+          legend.text=element_text(size=legend_size),
+          legend.title = element_blank(), # size=.5
+          axis.text = element_text(size = rel(0.8)), axis.ticks = element_line(colour = "black"), 
+          legend.key = element_rect(colour = "grey80"), 
+          legend.key.size = unit(.3, "cm"),
+          panel.background = element_rect(fill = "white", colour = NA), panel.border = element_rect(fill = NA,  colour = "grey50"), 
+  panel.grid.major = element_line(colour = "white", size = 0.2),
+  panel.grid.minor = element_line(colour = "white",  size = 0.5), 
+  strip.background = element_rect(fill = "grey80", colour = "grey50", size = 0.2))
+}
+
